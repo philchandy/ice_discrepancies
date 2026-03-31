0bc0d1cb632c07fbb56cc23f04031ee19cc879db
Phillip Chandy <phil@Phillips-MacBook-Air.local>
2026-03-31 11:24:41 -0400

    methodology component, footer fix

---
+++ src/components/Methodology.jsx
@@ -0,0 +1,24 @@
+export default function Methodology() {
+    return (
+        <div className="">
+            <section>
+                <h3>Methodology Notes</h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Corporis repellendus repudiandae animi aliquam nam perspiciatis fugit, non laborum soluta, 
+                id suscipit maiores molestiae similique praesentium quos inventore libero dolore laboriosam.
+                </p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Corporis repellendus repudiandae animi aliquam nam perspiciatis fugit, non laborum soluta, 
+                id suscipit maiores molestiae similique praesentium quos inventore libero dolore laboriosam.
+                </p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Corporis repellendus repudiandae animi aliquam nam perspiciatis fugit, non laborum soluta, 
+                id suscipit maiores molestiae similique praesentium quos inventore libero dolore laboriosam.
+                </p>
+            </section>
+        </div>
+    );
+}
