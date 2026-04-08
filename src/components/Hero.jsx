32f87233a30e0011e048971294cdfa2a58c4cd55
Phillip Chandy <pchandydev@gmail.com>
2026-04-07 20:05:38 -0400

    test1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,11 @@ export default function Hero() {
       <div className="hero-inner">
         <div>
           <div className="hero-kicker">CS7250</div>
-          <h1 className="hero-title">Inside NICK</h1>
+          <h1 className="hero-title">Inside the ICE Detention System</h1>
           <p className="hero-copy">
-            Exploring all of nicks insides!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Corporis repellendus repudiandae animi aliquam nam perspiciatis fugit, non laborum soluta, 
+            id suscipit maiores molestiae similique praesentium quos inventore libero dolore laboriosam.
           </p>
         </div>
       </div>
